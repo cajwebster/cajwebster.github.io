c44e44e5b765793cd7fb37a137f7786e5391a723
DachshundLord <49292497+DachshundLord@users.noreply.github.com>
2019-06-12 15:04:24 -0300

    Update game.js

--- firstGame/game.js
+++ firstGame/game.js
@@ -1,4 +1,22 @@
 var CANVAS_WIDTH = 480;
 var CANVAS_HEIGHT = 320;
 
-var canvasElement = $("<canvas width='" + 
+var canvasElement = $("<canvas width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "'></canvas>");
+var canvas = canvas.get(0).getContext("2d");
+canvasElement.appendTo("body");
+
+function update()
+{
+}
+
+function draw()
+{
+	canvas.fillStyle = "#000000";
+	canvas.fillText("Hello", 50, 50);
+}
+
+var FPS = 30;
+setInterval(function() {
+	update();
+	draw();
+}, 1000/FPS);
