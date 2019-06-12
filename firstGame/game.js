aa935e1f0e8f8a286f640a8cfec15dff2dad0b80
DachshundLord <49292497+DachshundLord@users.noreply.github.com>
2019-06-12 15:04:59 -0300

    Update game.js

--- firstGame/game.js
+++ firstGame/game.js
@@ -2,7 +2,7 @@ var CANVAS_WIDTH = 480;
 var CANVAS_HEIGHT = 320;
 
 var canvasElement = $("<canvas width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "'></canvas>");
-var canvas = canvas.get(0).getContext("2d");
+var canvas = canvasElement.get(0).getContext("2d");
 canvasElement.appendTo("body");
 
 function update()
